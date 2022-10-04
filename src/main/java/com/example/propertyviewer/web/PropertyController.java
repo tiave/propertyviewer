6c4011e68765ca62dae84709025291405ebcccfc
tiave <89769023+tiave@users.noreply.github.com>
2022-10-04 17:56:31 +0300

    features for adding and editing

--- src/main/java/com/example/propertyviewer/web/PropertyController.java
+++ src/main/java/com/example/propertyviewer/web/PropertyController.java
@@ -1,6 +1,8 @@
 package com.example.propertyviewer.web;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.propertyviewer.domain.Property;
@@ -27,6 +29,28 @@ public class PropertyController {
         public @ResponseBody List<Property> propertiesRest() {
             return (List<Property>) repo.findAll();
         }
+
+        //user will navigate to addproperty.html page by pressing "add property"
+        @RequestMapping(value="/add")
+        public String addProperty(Model model){
+            model.addAttribute("property", new Property());
+            return "addproperty";
+        }
+
+        //saving new property info, redirecting to properties.html
+        @RequestMapping(value="/save", method=RequestMethod.POST)
+        public String save(Property property){
+            repo.save(property);
+            return "redirect:properties";
+        }
+
+        //navigate to editproperty page based on id to edit the existing info
+        @RequestMapping(value="/edit/{id}", method=RequestMethod.GET)
+        public String editProperty(@PathVariable("id") Long id, Model model) {
+            model.addAttribute("property", repo.findById(id));
+            return "editproperty";
+        }
+
 
 
 }
